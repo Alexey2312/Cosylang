eb03226ff1f4cfb1793a7610e9608c2ff13b84a7
Alexey2312 <AlexeyNikon23@gmail.com>
2026-08-19 15:46:34 +0300

    Implement Node struct

---
+++ src/frontend/parser/node/node.hpp
@@ -0,0 +1,90 @@
+#pragma once
+#include "../../lexer/token/token.hpp"
+
+namespace Cosylang::Parser
+{
+
+enum class NodeType
+{
+    ID,
+    INTEGER,
+    FLOAT,
+    STRING,
+
+    TRUE, // true
+    FALSE, // false
+
+    DOT, // .
+    COMMA,// ,
+    COLON, // :
+    SEMICOLON, // ;
+    RANGE, // ..
+
+    ASSIGN, // =
+    PLUS_ASSIGN, // +=
+    MINUS_ASSIGN, // -=
+    MUL_ASSIGN, // *=
+    DIV_ASSIGN, // /=
+
+    MUL, // *
+    DIV, // /
+    PLUS, // +
+    MINUS, // -
+
+    EQUALS, // ==
+    NOT_EQUALS, // !=
+    GREATER_OR_EQUAL, // >=
+    LESS_OR_EQUAL, // <=
+    GREATER, // >
+    LESS, // <
+
+    CODE_BLOCK, // { }
+    ARRAY_INDEX, // [ ]
+    PARENTHESIS, // ( )
+
+    AND, // && or 'and'
+    OR, // || or 'or'
+    NOT, // ! or 'not'
+
+    BIT_AND, // &
+    BIT_OR, // |
+    BIT_XOR, // ^
+    BIT_NOT, // ~
+
+    IMPORT, // import
+    NAMESPACE, // namespace
+
+    VAR, // var
+    CONST, // const
+
+    IF, // if
+    ELSE, // else
+    ELIF, // elif
+
+    MATCH, // match
+
+    WHILE, // while
+    FOR, // for
+    CONTINUE, // continue
+    BREAK, // break
+
+    FUNC, // func
+    RETURN, // return
+    ARROW, // =>
+
+    BODY, // keyword 'body'
+
+    TYPE, // keyword 'type'
+};
+
+struct Node
+{
+    NodeType type;
+    const Lexer::Token::Token* token;
+
+    Node* first_child = nullptr;
+    Node* next_sibling = nullptr;
+};
+
+
+}; // namespace Cosylang::Parser
